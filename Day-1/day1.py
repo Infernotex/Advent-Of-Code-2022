e957b58cf8d7c8c4b74cffc7af556da45d1dc777
Infernotex <erik.tran.sv@gmail.com>
2022-12-01 15:25:05 +0100

    Day 1 completed

---
+++ Day-1/day1.py
@@ -0,0 +1,19 @@
+def main():
+    f = open("input", "r")
+    index: int = 0
+    elves: list[int] = [0] 
+
+    for calories in f:
+        if calories != "" and calories != "\n":
+            elves[index] += int(calories)
+        else:
+            index += 1
+            elves.append(0)
+
+    elves.sort()
+
+    print(f"The elf with the most calories carries: {elves[-1]} calories.")
+    print(f"The top 3 elves carries a total of: {elves[-1] + elves[-2] + elves[-3]}")
+
+if __name__ == "__main__":
+    main()
