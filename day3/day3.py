96efa46ac455845673ea7166ef75e40ccb71eac6
Infernotex <erik.tran.sv@gmail.com>
2022-12-04 21:33:08 +0100

    Day 3 tweaked

--- day3/day3.py
+++ day3/day3.py
@@ -1,5 +1,8 @@
 def letter_to_value(letter: str) -> int:
     value = ord(letter)
+    # ASCII values:
+    # A-Z: 65-90
+    # a-z: 97-122
     if value <= 90:
         return value - 38
     else:
@@ -10,46 +13,31 @@ def main() -> None:
     f = open("input", "r")
     rucksacks: list[str] = []
     priority_sum: int = 0
+    badge_sum: int = 0
 
     rucksack: str
     for rucksack in f:
-        rucksacks.append(rucksack[:-1])
+        rucksacks.append(rucksack[: -1])
 
-        item_found: bool = False
         cutoff_index: int = (len(rucksack) - 1) // 2
 
+        i: int
         for i in range(cutoff_index):
-            if item_found:
+            if rucksack[i] in rucksack[cutoff_index:]:
+                priority_sum += letter_to_value(rucksack[i])
                 break
-            else:
-                for j in range(cutoff_index):
-                    if rucksack[i] == rucksack[j + cutoff_index]:
-                        # ASCII values:
-                        # A-Z: 65-90
-                        # a-z: 97-122
-                        priority_sum += letter_to_value(rucksack[i])
-                        item_found = True
-                        break
 
     f.close()
 
-    badge_sum: int = 0
     i: int
     for i in range(2, len(rucksacks), 3):
         j: int = i - 1
         k: int = i - 2
-        item_found: bool = False
 
-        for a in rucksacks[i]:
-            for b in rucksacks[j]:
-                if item_found:
-                    break
-                else:
-                    for c in rucksacks[k]:
-                        if a == b == c:
-                            badge_sum += letter_to_value(a)
-                            item_found = True
-                            break
+        for letter in rucksacks[i]:
+            if letter in rucksacks[j] and letter in rucksacks[k]:
+                badge_sum += letter_to_value(letter)
+                break
 
     print(priority_sum)
     print(badge_sum)
